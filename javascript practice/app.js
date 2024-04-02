5b61d91a231908a960a1022495624f2102362057
Muhammad Subhan <112649712+firsttestgithub@users.noreply.github.com>
2024-04-02 20:09:20 +0500

    Add files via upload

---
+++ javascript practice/app.js
@@ -0,0 +1,13 @@
+/*let object = {
+	array : [],
+};
+		
+		let prom = +prompt("Enter your value");
+		for(let i = 1; i<=prom;i++){
+			let user = +prompt("Enter your data");
+			object.array.push(user);
+		console.log(user)
+		}*/
+		
+		 
+		 console.log(4+5);
